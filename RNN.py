5b73ca8000dc7e5744d53f58c28d6ebdac745c99
Bhairav Chidambaram <bchidamb@caltech.edu>
2018-03-05 23:08:00 -0800

    add data

---
+++ RNN.py
@@ -0,0 +1,10 @@
+import numpy as np
+from keras.models import Sequential
+from keras.models import Dense
+from keras.layers import LSTM
+from process_data import *
+
+def model():
+    # model = Sequential()
+    pass
+    
